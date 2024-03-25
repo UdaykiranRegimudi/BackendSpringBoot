2d8b446c36d639851e24b019e8f62fea8ea39256
UdaykiranRegimudi <uuday3804@gmail.com>
2024-03-25 09:25:14 +0000

    commit

---
+++ src/main/java/com/example/player/PlayerController.java
@@ -0,0 +1,38 @@
+
+package com.example.player;
+
+import org.springframework.web.bind.annotation.*;
+import java.util.ArrayList;
+
+@RestController
+
+public class PlayerController {
+
+    PlayerService playerService = new PlayerService();
+
+    @GetMapping("/players")
+    public ArrayList<Player> getPlayers() {
+        return playerService.getPlayers();
+    }
+
+    @PostMapping("/players")
+    public Player addPlayer(@RequestBody Player player) {
+        return playerService.addPlayer(player);
+    }
+
+    @GetMapping("/players/{playerId}")
+    public Player getPlayerById(@PathVariable("playerId") int playerId) {
+        return playerService.getPlayerById(playerId);
+    }
+
+    @PutMapping("/players/{playerId}")
+    public Player updatePlayer(@PathVariable("playerId") int playerId, @RequestBody Player player) {
+        return playerService.updatePlayer(playerId, player);
+    }
+
+    @DeleteMapping("/players/{playerId}")
+    public void deleteBook(@PathVariable("playerId") int playerId) {
+        playerService.deletePlayer(playerId);
+    }
+
+}
